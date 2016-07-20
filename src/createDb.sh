9a463f623f1deb7c66a139b3ec7852e2cad49a79
Daniel Owen <jayoshi32@gmail.com>
2016-07-20 16:10:38 +0700

    Capabilities are WORKING!!!
    Finally! everything is now compatible with python 3, and it works

--- src/createDb.sh
+++ src/createDb.sh
@@ -1,4 +1,5 @@
 # using a user with createdb and dropdb roles, or the user postgres
 dropdb languagedepot-metadata --if-exists
 createdb languagedepot-metadata
+#./createdb.py
 psql languagedepot-metadata < languagedepot-metadata.sql
