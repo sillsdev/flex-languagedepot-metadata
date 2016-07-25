7899085ba6583e28333aa387efeb5be73fe5ad8e
megahirt <chris@hirtfamily.net>
2016-07-25 14:46:02 +0700

    Make runAnalysis accept a data folder on the command line

--- src/runAnalysis.py
+++ src/runAnalysis.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import os
+import os,sys
 from LanguageDepotAnalyze import Runner
 
 # config file is specified, then used for the program!
 cfgName = 'config.json'
 dataPath = '../data'
+if len(sys.argv) == 2:
+    dataPath = sys.argv[1]
+print("Analyzing directory %s\n" % dataPath)
 runner = Runner(cfgName, dataPath)
 runner.run()
