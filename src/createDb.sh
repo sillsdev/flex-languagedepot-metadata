24a9aa200625b97e2b53c9c02609b76bce96e8bd
Nathaniel Paulus <nateowami@gmail.com>
2016-12-02 16:01:53 +0700

    Improve functionality of createDb.sh and saveAsCSV.sh

--- src/createDb.sh
+++ src/createDb.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+# cd to the directory of this script
+cd "$(dirname "$0")" || exit
+
 # using a user with createdb and dropdb roles, or the user postgres
-echo Emptying languagedepot-metadata database
+echo "Emptying the languagedepot-metadata database."
+
 dropdb languagedepot-metadata --if-exists
 createdb languagedepot-metadata
+
 python3 createDb.py
 psql languagedepot-metadata < languagedepot-metadata.sql
-echo Done
+
+echo Finished
--- src/saveAsCSV.sh
+++ src/saveAsCSV.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Exports the database to a CSV file. The default file name is export.csv. This
+# may be overridden by passing the desired file name as an argument.
+
+OUTPUT=${1:-"export.csv"}
+
 # makes a csv
-echo Exporting table project.metadata to file export.csv
-psql languagedepot-metadata -c "\copy (SELECT * FROM project.metadata ORDER BY id) TO STDOUT WITH CSV HEADER" > export.csv
+echo Exporting table project.metadata to file $OUTPUT
+psql languagedepot-metadata -c "\copy (SELECT * FROM project.metadata ORDER BY id) TO STDOUT WITH CSV HEADER" > $OUTPUT
