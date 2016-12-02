6f91183d6b828bb004b78ee66f82d7b816a806ca
Nathaniel Paulus <nateowami@gmail.com>
2016-12-02 15:58:41 +0700

    Fix setupPostgresql.sh and improve its instructions

--- src/setupPostgresql.sh
+++ src/setupPostgresql.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-DIR=`dirname "$(readlink -f $0)"`
+cd "$(dirname "$0")" || exit
+
+echo "Setting up a database user."
+echo "You may be prompted for the password for the $(whoami) user."
+echo "As you type the password no output will be shown."
+echo
 
 sudo -u postgres psql -c "CREATE ROLE $USERNAME WITH LOGIN CREATEDB PASSWORD 'placeholder'" 2> /dev/null
-createdb $USERNAME
 
 # Replaces "USERNAME" with the actual username in config.json
-sed -i "s/USERNAME/$USERNAME/" $DIR/config.json
+sed -i "s/USERNAME/$USERNAME/" config.json
+
+echo Finished
