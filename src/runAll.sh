1906912a2e17f5b810a14540c6a23739ba65d0e6
Nathaniel Paulus <nateowami@gmail.com>
2016-12-02 16:49:14 +0700

    Add runAll.sh as a convenience script

---
+++ src/runAll.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run everything necessary to get an output file in one command
+
+ORIGINAL_CWD=`pwd`
+
+# cd to the directory of this script
+cd "$(dirname "$0")" || exit
+
+./installDependencies.sh --quiet
+./setupPostgresql.sh
+./createDb.sh
+./runAnalysis.py
+
+DIR=`pwd`
+
+# Go back to the original CWD so the file will be saved in the right place
+
+cd $ORIGINAL_CWD || exit
+
+exec "$DIR/saveAsCSV.sh" $1
